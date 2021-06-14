1984b413a7e9c994b59a6e6dcc7d6f87cff4cd33
Lorena C <lorena@eunknown.org>
2021-06-14 14:54:26 -0700

    inital commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Main from './Main';
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      Notes App
+      <Sidebar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
