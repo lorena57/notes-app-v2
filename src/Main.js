8d86434c6aca3f4f7dcaf7edfb364fc2a2cc5129
Lorena C <lorena@eunknown.org>
2021-06-14 16:20:58 -0700

    add main css

--- src/Main.js
+++ src/Main.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 const Main = () => {
   return (
-    <div>
-      <p>Main</p>
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input type="text" id="title" autoFocus />
+        <textarea
+          id="body"
+          placeholder="Write your note here..."
+          cols="30"
+          rows="10"
+        ></textarea>
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">Title</h1>
+        <div className="markdown-preview">Note Preview</div>
+      </div>
     </div>
   );
 };
