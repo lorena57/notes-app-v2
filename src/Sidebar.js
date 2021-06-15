bbaf42d5900a7a2357747de64bd95737e7ea0e3b
Lorena C <lorena@eunknown.org>
2021-06-15 13:28:36 -0700

    add time feature to sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -8,15 +8,22 @@ const Sidebar = ({ notes, onAddNote }) => {
         <button onClick={onAddNote}>Add</button>
       </div>
       <div className="app-sidebar-notes">
-        {notes.map((notes) => (
+        {notes.map((note) => (
           <div className="app-sidebar-note">
             <div className="sidebar-note-title">
-              <strong>Title</strong>
+              <strong>{note.title}</strong>
               <button>Delete</button>
             </div>
 
-            <p>Note Preview</p>
-            <small className="note-meta">Last modified [date]</small>
+            <p>{note.body && note.body.substr(0, 100) + '...'}</p>
+
+            <small className="note-meta">
+              Last modified{' '}
+              {new Date(note.lastModified).toLocaleDateString('en-US', {
+                hour: '2-digit',
+                minute: '2-digit',
+              })}
+            </small>
           </div>
         ))}
       </div>
